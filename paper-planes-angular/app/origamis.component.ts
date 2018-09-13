c06abd5a2a0a30a8cefaf7e515f822289cb9d386
Unknown <xavier.ozarowski.14@eigsi.fr>
2018-09-13 16:10:42 +0200

    origamis

--- paper-planes-angular/app/origamis.component.ts
+++ paper-planes-angular/app/origamis.component.ts
@@ -9,5 +9,18 @@ import { Component } from '@angular/core';
 	})
 
 export class OrigamisComponent {
+  private origami: Origami = new Origami();
+  private origamis: Array<Origami> = new Array<Origami>();
 
+  constructor() {
+      this.origamis.push(new Origami("Lapin", "Normal", 10, 3, "blabla"));
+  }
+
+  public filtrerOrigamis(): Array<Origami> {
+      return this.origamis.filter(o =>
+          o.getNom()
+              .toLowerCase()
+              .indexOf(this.filtre.toLowerCase()) !== -1
+      )
+  }
 }
