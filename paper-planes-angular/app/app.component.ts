2c79e664c511f11e59d914157da7ead2b3162ba6
Unknown <aastruc86@gmail.com>
2018-09-13 12:32:57 +0200

    commit arnaud

--- paper-planes-angular/app/app.component.ts
+++ paper-planes-angular/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'paperplane-app',
     templateUrl: 'app/app.component.html',
-    styleUrls: ['app/app.component.css']  
+    styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
 
