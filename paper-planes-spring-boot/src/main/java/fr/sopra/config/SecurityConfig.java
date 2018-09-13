aafa102d103d48f937db1f26f51c5b51be157e47
Unknown <samsamebe@gmail.com>
2018-09-13 11:34:26 +0200

    Import Security

--- paper-planes-spring-boot/src/main/java/fr/sopra/config/SecurityConfig.java
+++ paper-planes-spring-boot/src/main/java/fr/sopra/config/SecurityConfig.java
@@ -1,5 +1,16 @@
 package fr.sopra.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
