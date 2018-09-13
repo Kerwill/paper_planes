e043f1fcf2e6a3e7aeccc5c698635c99609925ed
Unknown <audeveniere@outlook.fr>
2018-09-13 11:29:41 +0200

    Security & administrateur

---
+++ paper-planes-spring-boot/src/main/java/fr/sopra/AuthService.java
@@ -0,0 +1,27 @@
+package fr.sopra;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import fr.sopra.dao.IDAOAdministrateur;
+import fr.sopra.model.Administrateur;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+	@Autowired
+	IDAOAdministrateur daoAdministrateur;
+
+//	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		return new UtilisateurPrincipal(this.daoUtilisateur.findByUsername(username));
+		
+		Administrateur myAdministrateur = this.daoAdministrateur.findByUsername(username);
+		AdministrateurPrincipal myPrincipal = new AdministrateurPrincipal(myAdministrateur);
+		return myPrincipal;
+		
+	}
+}
