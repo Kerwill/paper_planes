c80bfc8de97c7939e5cd21ad1a0e3c3dd7e62e07
Unknown <guillaume.dreno@gmail.com>
2018-09-13 16:37:05 +0200

    Home Controlle, à vérifier

--- paper-planes-spring-boot/src/main/java/fr/sopra/controller/HomeController.java
+++ paper-planes-spring-boot/src/main/java/fr/sopra/controller/HomeController.java
@@ -2,14 +2,19 @@ package fr.sopra.controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 public class HomeController {
 	
 	@GetMapping("/")
-	public String home(@RequestParam(defaultValue = "visiteur") String username, Model model) {
+	public String home(String username, Model model) {
 		model.addAttribute("administrateur", username);
 		return "home";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 }
 
