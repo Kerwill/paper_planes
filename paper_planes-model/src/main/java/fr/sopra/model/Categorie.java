e2dac4297326a4a6cd1be9da4144c3c32e67154f
Emma Dikongue <emmadikongue@gmail.com>
2018-09-13 11:01:29 +0200

    IDAO origami

--- paper_planes-model/src/main/java/fr/sopra/model/Categorie.java
+++ paper_planes-model/src/main/java/fr/sopra/model/Categorie.java
@@ -14,36 +14,37 @@ public class Categorie {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="CAT_ID")
+	@Column(name = "CAT_ID")
 	private int id;
 
-    @Column(name="CAT_NOM")
-    private String nom;
+	@Column(name = "CAT_NOM")
+	private String nom;
 
 	@ManyToMany
 	private Set<Origami> origamis;
 
-
-
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-  public Set<Origami> getOrigamis() {
-    return origamis;
-  }
-  public void setOrigamis(Set<Origami> origamis) {
-    this.origamis = origamis;
-  }
+
+	public Set<Origami> getOrigamis() {
+		return origamis;
+	}
+
+	public void setOrigamis(Set<Origami> origamis) {
+		this.origamis = origamis;
+	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
-
 
 }
