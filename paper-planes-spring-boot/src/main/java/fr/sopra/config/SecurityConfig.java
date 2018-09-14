2cb2b862efe8a29bacf1c454ceafc3ccfe3e390d
Unknown <samsamebe@gmail.com>
2018-09-14 11:19:48 +0200

    nav

--- paper-planes-spring-boot/src/main/java/fr/sopra/config/SecurityConfig.java
+++ paper-planes-spring-boot/src/main/java/fr/sopra/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
 				.formLogin()
 					.loginPage("/login")
 					.loginProcessingUrl("/perform_login")
-					.defaultSuccessUrl("/administration/home-admin", true)
+					.defaultSuccessUrl("/home", true)
 					.failureUrl("/login?error=true")
 					.permitAll()
 				.and()
