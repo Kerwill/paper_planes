31657491acdc50cf4b7ade3d5708a9a0b4f1201b
Blandel <leila.blandel@gmail.com>
2018-09-14 10:47:04 +0200

    commit leila

--- paper-planes-angular/app/etape.service.ts
+++ paper-planes-angular/app/etape.service.ts
@@ -13,12 +13,10 @@ constructor(private http: Http, private appConfigService: AppConfigService) {
 // myHeaders.append('Authorization', this.appConfigService.getApiBasic());
 // this.requestOptions = new RequestOptions({ headers: myHeaders });
    this.http
-        .get(this.appConfigService.getUrlApi() + "etapes")
+        .get(this.appConfigService.getUrlApi() + "etapes/search/by-origami-id?id=1")
         .subscribe(resp => {
             for (let e of resp.json()._embedded.etapes) {
                 this.etapes.push(new Etape(e));
-
-            console.log("ok service");
             }
         });
 }
