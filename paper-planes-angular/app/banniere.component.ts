eee7cad6eca7e9aa937f629bfc1f03494dd5f98a
Unknown <guillaume.dreno@gmail.com>
2018-09-15 10:58:06 +0200

    banniere en construction

---
+++ paper-planes-angular/app/banniere.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+//import service si besoin
+//import classe model si besoin
+
+@Component({
+    selector: 'myBanniere',
+    templateUrl: 'app/banniere.component.html',
+    styleUrls: ['app/banniere.component.css']
+	})
+
+export class BanniereComponent {
+
+}
