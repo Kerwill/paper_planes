1d96a5fae40c99ae5db41ad975ccfd386f6a3caa
Unknown <aastruc86@gmail.com>
2018-09-17 09:27:29 +0200

    commit arnaud

--- paper-planes-spring-boot/src/main/java/fr/sopra/dao/IDAOOrigami.java
+++ paper-planes-spring-boot/src/main/java/fr/sopra/dao/IDAOOrigami.java
@@ -23,17 +23,17 @@ public interface IDAOOrigami extends JpaRepository<Origami, Integer> {
 	@RestResource(path = "by-nom-containing")
 	public List<Origami> findByNomContaining(@Param("nom") String nom);
 
-	@RestResource(path = "by-categorie")
-	public List<Origami> findByCategorieNom(@Param("categorie") String nom);
+//	@RestResource(path = "by-categorie")
+//	public List<Origami> findByCategorieNom(@Param("categorie") String nom);
 
 
-@RestResource(path="by-filtre")
-@Query(value = "SELECT o FROM Origami o"
-		+ "WHERE o.name LIKE '%:filtreRecherche' "
-		+ "AND :selectedCategorie MEMBER OF o.categories"
-		+ "AND"
-		+ "((o.niveau = Occasionnel) IS :isOccasionnel OR (o.niveau = Normal) IS :isNormal OR (o.niveau = Avance) IS :isAvance OR (o.niveau = Expert) IS :isExpert")
-public List<Origami> findByFiltration(@Param("filtreRecherche") String filtreRecherche,@Param("selectedCategories") Categorie selectedCategorie, @Param("isOccasionnel") Boolean isOccasionnel,
-		@Param("isNormal") Boolean isNormal, @Param("isAvance") Boolean isAvance, @Param("isExpert") Boolean isExpert);
+//@RestResource(path="by-filtre")
+//@Query(value = "SELECT o FROM Origami o"
+//		+ "WHERE o.name LIKE '%:filtreRecherche' "
+//		+ "AND :selectedCategorie MEMBER OF o.categories"
+//		+ "AND"
+//		+ "((o.niveau = Occasionnel) IS :isOccasionnel OR (o.niveau = Normal) IS :isNormal OR (o.niveau = Avance) IS :isAvance OR (o.niveau = Expert) IS :isExpert")
+//public List<Origami> findByFiltration(@Param("filtreRecherche") String filtreRecherche,@Param("selectedCategories") Categorie selectedCategorie, @Param("isOccasionnel") Boolean isOccasionnel,
+//		@Param("isNormal") Boolean isNormal, @Param("isAvance") Boolean isAvance, @Param("isExpert") Boolean isExpert);
 
 }
